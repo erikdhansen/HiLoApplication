36edf6c49c29f7af4c8743ed23a01bd6dd7353aa
Erik Hansen <erikdhansen@mac.com>
2018-06-18 11:02:26 -0400

    Modify header file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
- * Author: ehansen
  *
  * Created on June 17, 2018, 4:11 PM
  */
